20897f6e4c438dcb4b07b83d7bd55de1489fb79f
hjpwhu <hjpwhu@qq.com>
2017-05-21 16:26:34 +0800

    update beginner tutorial

---
+++ src/hjp.edu.torch.man.demo/MA0503.py
@@ -0,0 +1,110 @@
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+torch.manual_seed(1)
+
+word_to_ix = {"hello": 0, "world": 1}
+embeds = nn.Embedding(2, 5)
+lookup_tensor = torch.LongTensor([word_to_ix["hello"]])
+hello_embed = embeds(autograd.Variable(lookup_tensor))
+print(hello_embed)
+
+CONTEXT_SIZE = 2
+EMBEDDING_DIM = 10
+
+test_sentence = """When forty winters shall besiege thy brow,
+And dig deep trenches in thy beauty's field,
+Thy youth's proud livery so gazed on now,
+Will be a totter'd weed of small worth held:
+Then being asked, where all thy beauty lies,
+Where all the treasure of thy lusty days;
+To say, within thine own deep sunken eyes,
+Were an all-eating shame, and thriftless praise.
+How much more praise deserv'd thy beauty's use,
+If thou couldst answer 'This fair child of mine
+Shall sum my count, and make my old excuse,'
+Proving his beauty by succession thine!
+This were to be new made when thou art old,
+And see thy blood warm when thou feel'st it cold.""".split()
+
+trigrams = [([test_sentence[i], test_sentence[i + 1]], test_sentence[i + 2]) 
+            for i in range(len(test_sentence) - 2)]
+print(trigrams)
+
+vocab = set(test_sentence)
+word_to_ix = {word: i for i, word in enumerate(vocab)}
+
+class NGramLanguageModeler(nn.Module):
+    
+    def __init__(self, vocab_size, embedding_dim, context_size):
+        super(NGramLanguageModeler, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.liner1 = nn.Linear(context_size * embedding_dim, 128)
+        self.liner2 = nn.Linear(128, vocab_size)
+        
+    def forward(self, inputs):
+        embeds = self.embeddings(inputs).view(1, -1)
+        out = F.relu(self.liner1(embeds))
+        out = self.liner2(out)
+        log_probs = F.log_softmax(out)
+        return log_probs
+    
+losses = []
+loss_function = nn.NLLLoss()
+model = NGramLanguageModeler(len(vocab), EMBEDDING_DIM, CONTEXT_SIZE)
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+for epoch in range(10):
+    total_loss = torch.Tensor([0])
+    
+    for context, target in trigrams:
+         context_idxs = [word_to_ix[w] for w in context]
+         context_var = autograd.Variable(torch.LongTensor(context_idxs))
+         
+         model.zero_grad()
+         
+         log_probs = model(context_var)
+         
+         loss = loss_function(log_probs, autograd.Variable(
+             torch.LongTensor([word_to_ix[target]])))
+         
+         loss.backward() 
+         optimizer.step()
+         
+         total_loss += loss.data
+    losses.append(total_loss)
+print(losses)
+
+CONTEXT_SIZE = 2
+raw_text = """We are about to study the idea of a computational process.
+Computational processes are abstract beings that inhabit computers.
+As they evolve, processes manipulate other abstract things called data.
+The evolution of a process is directed by a pattern of rules
+called a program. People create programs to direct processes. In effect,
+we conjure the spirits of the computer with our spells.""".split()
+word_to_ix = {word: i for i, word in enumerate(raw_text)}
+data = []
+for i in range(2, len(raw_text) - 2):
+    context = [raw_text[i - 2], raw_text[i - 1],
+               raw_text[i + 1], raw_text[i + 2]]
+    target = raw_text[i]
+    data.append((context, target))
+print(data[:5])
+
+class CBOW(nn.Module):
+    
+    def __init__(self):
+        pass
+    
+    def forward(self, inputs):
+        pass
+    
+def make_context_vector(context, word_to_ix):
+    idxs = [word_to_ix[w] for w in context]
+    tensor = torch.LongTensor(idxs)
+    return autograd.Variable(tensor)
+
+make_context_vector(data[0][0], word_to_ix)
